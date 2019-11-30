9d34a92d30c8744b7ad30c826c1bb38969b1e5d5
Jfs <jf.stassen@gmail.com>
2019-11-30 17:17:46 +0100

    modify span JokeList

--- src/JokeList.js
+++ src/JokeList.js
@@ -96,7 +96,7 @@ class JokeList extends Component {
             <div className="JokeList">
                 <div className="JokeList-sidebar">
                     <h1 className="JokeList-title">
-                        <span>Dad</span> Jokes
+                        <span>Dad</span> <span>Mika</span> Jokes
                     </h1>
                     <img
                         src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
